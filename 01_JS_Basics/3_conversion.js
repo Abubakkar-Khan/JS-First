d63d979c123592704dfb2f972e27e356d33c6e1a
Abubakar Khan <abubakar.khan.official001@gmail.com>
2024-08-24 13:50:57 +0000

    conversion of Data types

---
+++ 01_JS_Basics/3_conversion.js
@@ -0,0 +1,15 @@
+let score = null
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let value_number = Number(score);
+
+console.log(typeof (value_number));
+console.log(value_number);
+
+let bool = -null
+let is_bool = Boolean(bool)
+console.log(typeof is_bool);
+console.log(is_bool);
+
