6e241fed2ab1b484e8e62a5f4a1fd9a94268b357
Abubakar Khan <abubakar.khan.official001@gmail.com>
2024-08-25 19:40:42 +0000

    data types summery

---
+++ 01_JS_Basics/5_Data_types_summary.js
@@ -0,0 +1,24 @@
+const id = Symbol("123")
+const NEW_id = Symbol("123")
+
+// console.log(id)
+// console.log(NEW_id)
+
+let bit_int = 123123123123123n
+
+const array = ["abc", "123", "qwe"]
+const n_array = [1, 23, 122]
+console.log(n_array)
+
+let m_obj = {
+    name: "Ali",
+    age: 22
+};
+ console.log(m_obj)
+ 
+ const m_fun = function () {
+     console.log(typeof array)
+}
+
+m_fun()
+
